021de1aacac03df960efc5e2f68cb9b280ca6050
Samuel Gómez Balderas <sierra_034@outlook.com>
2018-11-26 01:52:50 -0600

    Modelo Auto

---
+++ php/model/Auto.php
@@ -0,0 +1,19 @@
+<?php 
+class Auto {
+	private $id_auto;
+	private $precio;
+	private $cilindrada;
+	private $id_modelo;
+
+	function set_id_auto($idAuto) { $this->id_auto = $idAuto; }
+	function set_precio($precio) { $this->precio = $precio; }
+	function set_cilindrada($cilindrada) { $this->cilindrada = $cilindrada; }
+	function set_id_modelo($idModelo) { $this->id_modelo = $idModelo; }
+
+	function get_id_auto() { return $this->id_auto; }
+	function get_precio() { return $this->precio = $precio; }
+	function get_cilindrada() { return $this->cilindrada; }
+	function get_id_modelo() { return $this->id_modelo; }
+}
+
+ ?>
