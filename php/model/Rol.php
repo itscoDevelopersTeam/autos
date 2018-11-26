63ac53dac109c52725ff5547c3d063dd6f19537f
Samuel Gómez Balderas <sierra_034@outlook.com>
2018-11-25 23:47:59 -0600

    Modelo Rol

---
+++ php/model/Rol.php
@@ -0,0 +1,22 @@
+<?php 
+class Rol {	
+	private $id_rol;
+	private $nombre;
+	private $descripcion;
+
+	function __construct($idRol, $nombre, $descripcion) {
+		$this->id_rol = $idRol;
+		$this->nombre = $nombre;
+		$this->descripcion = $descripcion;
+	}
+
+	function set_id_rol($idRol) { $this->id_rol = $idRol; }
+	function set_nombre($nombre) { $this->nombre = $nombre; }
+	function set_descripcion($descripcion) { $this->descripcion = $descripcion; }
+
+	function get_id_rol() { return $this->id_rol; }
+	function get_nombre() { return $this->nombre; }
+	function get_descripcion() { return $this->descripcion; }
+}
+
+ ?>
